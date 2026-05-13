904053675c8d315c3c75472e73b8e7b95f996fc1
jose marcos <marcosads2025@gmail.com>
2026-05-13 09:52:10 -0300

    criando a classe Horario e o status horario

---
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/model/Horario.java
@@ -0,0 +1,38 @@
+package br.com.github.renato28.agendamentoaulaapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "horarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Horario {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "professor_id", nullable = false)
+    private Usuario professor;
+
+    @Column(nullable = false)
+    private LocalDateTime fim;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private StatusHorario statusHorario;
+
+
+}
+
+
+
---
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/model/StatusHorario.java
@@ -0,0 +1,6 @@
+package br.com.github.renato28.agendamentoaulaapi.model;
+
+public enum StatusHorario {
+        DISPONIVEL,
+        OCUPADO;
+}
