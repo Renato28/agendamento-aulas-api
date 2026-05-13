86b666f88ab8b28cd575fe0d0eaf0e8fca07f3b5
jose marcos <marcosads2025@gmail.com>
2026-05-13 15:05:24 -0300

    criando Dto para entidade usuario

---
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/dto/UsuarioRequestDto.java
@@ -0,0 +1,24 @@
+package br.com.github.renato28.agendamentoaulaapi.dto;
+
+import br.com.github.renato28.agendamentoaulaapi.model.Perfil;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UsuarioRequestDto {
+
+    @NotBlank(message = "O nome do usuário é obrigatório")
+    private String nome;
+    @NotBlank(message = "O e-mail do usuário é obrigatório")
+    @Email(message = "Formato inválido")
+    private String email;
+    @NotBlank(message = "A senha do usuário é obrigatória")
+    private String senha;
+    @NotBlank(message = "O perfil do usuário é obrigatória")
+    private Perfil perfil;
+}
