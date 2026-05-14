fced8234ebf5738c3b2a56c8e51e3d9b91bd080a
jose marcos <marcosads2025@gmail.com>
2026-05-14 14:44:29 -0300

    criando Dto da entidade  AgendamentoRequestDTO

---
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/dto/AgendamentoRequestDTO.java
@@ -0,0 +1,31 @@
+package br.com.github.renato28.agendamentoaulaapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AgendamentoRequestDTO {
+    @NotNull(message = "O ID do aluno é  obrigatório")
+    private Long alunoId;
+
+    @NotNull(message = "O ID professor do é obrigatório")
+    private Long professorId;
+
+    @NotNull(message = "O ID do curso é obrigatório")
+    private Long cursoId;
+
+    @NotNull(message = "O status do agendamento é  obrigatório")
+    private Long statusId;
+
+
+    private String linkReuniao;
+
+
+
+}
