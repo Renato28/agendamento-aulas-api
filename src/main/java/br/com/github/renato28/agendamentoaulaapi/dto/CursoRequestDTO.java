72980283f2bcb149e7449717a5f85bb61057a357
jose marcos <marcosads2025@gmail.com>
2026-05-14 14:45:43 -0300

    criando Dto da entidade  CursoRequestDTORequestDTO

--- src/main/java/br/com/github/renato28/agendamentoaulaapi/dto/CursoRequestDTO.java
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/dto/CursoRequestDTO.java
@@ -1,19 +1,28 @@
 package br.com.github.renato28.agendamentoaulaapi.dto;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 
 public class CursoRequestDTO {
 
-    @NotNull(message = "O titulo é obrigatório")
-    private String titulo;
+    @NotBlank(message = "O nome do curso é obrigatório")
+    private String nome;
 
     private String descricao;
 
 
-    @NotNull(message = "A duaração é obrigatória")
+    @NotNull(message = "A duração é obrigatória")
     @Positive(message="A duração deve ser maior que zero")
 
     private Integer duracao;
