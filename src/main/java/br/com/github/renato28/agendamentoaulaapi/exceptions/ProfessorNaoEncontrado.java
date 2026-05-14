7913bbcd4f8fdb8dbe629ea1a6ecffd19303234c
jose marcos <marcosads2025@gmail.com>
2026-05-14 15:56:17 -0300

    criando cLasse excepions professor

---
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/exceptions/ProfessorNaoEncontrado.java
@@ -0,0 +1,10 @@
+package br.com.github.renato28.agendamentoaulaapi.exceptions;
+
+
+public class ProfessorNaoEncontrado extends RuntimeException {
+    public ProfessorNaoEncontrado(String message) {
+        super(message);
+    }
+
+
+}
