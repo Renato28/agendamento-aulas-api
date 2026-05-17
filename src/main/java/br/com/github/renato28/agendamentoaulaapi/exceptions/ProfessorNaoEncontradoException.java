e507dbb71e46f53ad4fbb50c4d94b3afc6971ebb
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-17 14:30:15 -0300

    renomeando a classe de exception

---
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/exceptions/ProfessorNaoEncontradoException.java
@@ -0,0 +1,10 @@
+package br.com.github.renato28.agendamentoaulaapi.exceptions;
+
+
+public class ProfessorNaoEncontradoException extends RuntimeException {
+    public ProfessorNaoEncontradoException(String message) {
+        super(message);
+    }
+
+
+}
