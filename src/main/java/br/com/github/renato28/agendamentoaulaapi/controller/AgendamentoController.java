fbd900d9483262de0d85bdbab13584d7e5dc307e
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-21 19:51:42 -0300

    implementando metodo no controller para cancelar agendamento

--- src/main/java/br/com/github/renato28/agendamentoaulaapi/controller/AgendamentoController.java
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/controller/AgendamentoController.java
@@ -5,9 +5,9 @@ import br.com.github.renato28.agendamentoaulaapi.service.AgendamentoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -29,6 +29,13 @@ public class AgendamentoController {
         return ResponseEntity
                 .status(HttpStatus.CREATED).build();
 
+    }
+
+    @PutMapping("/cancelar/{agendamentoId}")
+    public ResponseEntity<Map<String, String>> cancelar(@PathVariable Long agendamentoId) {
+
+        agendamentoService.cancelar(agendamentoId);
+        return ResponseEntity.ok(Map.of("message", "Agendamento atualizado com sucesso"));
     }
 
 
