4ef72bf1ff55eedee89592233571a31ee1134d38
jose marcos <marcosads2025@gmail.com>
2026-05-24 21:20:53 -0300

    criando  a regra de negócio  atualizar no servicecurso

--- src/main/java/br/com/github/renato28/agendamentoaulaapi/service/CursoService.java
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/service/CursoService.java
@@ -1,13 +1,21 @@
 package br.com.github.renato28.agendamentoaulaapi.service;
 
 import br.com.github.renato28.agendamentoaulaapi.dto.CursoRequestDTO;
+import br.com.github.renato28.agendamentoaulaapi.exceptions.CursoNaoEncontradoException;
 import br.com.github.renato28.agendamentoaulaapi.exceptions.ProfessorNaoEncontradoException;
 import br.com.github.renato28.agendamentoaulaapi.model.Curso;
 import br.com.github.renato28.agendamentoaulaapi.model.Usuario;
 import br.com.github.renato28.agendamentoaulaapi.repository.CursoRepository;
 import br.com.github.renato28.agendamentoaulaapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -28,6 +36,33 @@ public class CursoService {
                 .preco(dto.getPreco()).professor(professor).build();
         cursoRepository.save(curso);
     }
+
+    @Transactional
+    public void atualizar(Long agendamentoId, CursoRequestDTO dto) {
+
+        // Busca o agendamento pelo ID
+        Curso curso = cursoRepository.findById(agendamentoId)
+                .orElseThrow(() ->
+                        new CursoNaoEncontradoException("curso não encontrado"));
+
+        Usuario professor = usuarioRepository
+                .findById(dto.getProfessorId())
+                .orElseThrow(() ->
+                        new ProfessorNaoEncontradoException("professor não encontrado"));
+
+
+        curso.setNome(dto.getNome());
+        curso.setDescricao(dto.getDescricao());
+        curso.setDuracao(dto.getDuracao());
+        curso.setPreco(dto.getPreco());
+        curso.setProfessor(professor);
+
+        cursoRepository.save(curso);
+
+
+    }
+
+
 
 
 }
