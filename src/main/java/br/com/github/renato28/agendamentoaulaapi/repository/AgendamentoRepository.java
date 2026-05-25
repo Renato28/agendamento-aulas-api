191ea28abd9c283309568d790d3a0e89d62f4afe
jose marcos <marcosads2025@gmail.com>
2026-05-25 15:42:37 -0300

    criando a query no meu repository agendamento buscar agendamento completo por id , e listar todos os agendamentos

--- src/main/java/br/com/github/renato28/agendamentoaulaapi/repository/AgendamentoRepository.java
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/repository/AgendamentoRepository.java
@@ -4,13 +4,14 @@ import br.com.github.renato28.agendamentoaulaapi.model.Agendamento;
 import br.com.github.renato28.agendamentoaulaapi.model.StatusAgendamento;
 import br.com.github.renato28.agendamentoaulaapi.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
-
 
     List<Agendamento> findByAluno(Usuario aluno);
 
@@ -21,4 +22,29 @@ public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> 
     List<Agendamento> findByAlunoAndStatus(Usuario aluno, StatusAgendamento status);
 
     List<Agendamento> findByProfessorAndStatus(Usuario professor, StatusAgendamento status);
+
+
+    // Buscar agendamento completo por ID
+    @Query("""
+           SELECT a
+           FROM Agendamento a
+           JOIN FETCH a.aluno
+           JOIN FETCH a.professor
+           JOIN FETCH a.curso
+           JOIN FETCH a.horario
+           WHERE a.id = :id
+           """)
+    Optional<Agendamento> buscarCompletoPorId(Long id);
+
+
+    // Listar todos os agendamentos completos
+    @Query("""
+           SELECT a
+           FROM Agendamento a
+           JOIN FETCH a.aluno
+           JOIN FETCH a.professor
+           JOIN FETCH a.curso
+           JOIN FETCH a.horario
+           """)
+    List<Agendamento> listarCompleto();
 }
