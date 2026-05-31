31f19d66fcabff5f10d05e345dfeb74349ca0b09
jose marcos <marcosads2025@gmail.com>
2026-05-31 10:25:55 -0300

    criando serviço atualizar DisponibilidadeService

--- src/main/java/br/com/github/renato28/agendamentoaulaapi/service/DisponibilidadeService.java
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/service/DisponibilidadeService.java
@@ -2,10 +2,13 @@ package br.com.github.renato28.agendamentoaulaapi.service;
 
 import br.com.github.renato28.agendamentoaulaapi.dto.DisponibilidadeRequestDTO;
 import br.com.github.renato28.agendamentoaulaapi.exceptions.DisponibilidadeNaoEncontradoException;
+import br.com.github.renato28.agendamentoaulaapi.exceptions.RegraDeNegocioException;
+import br.com.github.renato28.agendamentoaulaapi.exceptions.UsuarioNaoEncontradoException;
 import br.com.github.renato28.agendamentoaulaapi.model.Disponibilidade;
 import br.com.github.renato28.agendamentoaulaapi.model.Usuario;
 import br.com.github.renato28.agendamentoaulaapi.repository.DisponibilidadeRepository;
 import br.com.github.renato28.agendamentoaulaapi.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +32,56 @@ public class DisponibilidadeService {
                 .duracaoAula(dto.getDuracaoAula())
                 .build();
 
-       disponibilidadeRepository.save(disponibilidade);
+        disponibilidadeRepository.save(disponibilidade);
+    }
+
+    @Transactional
+    public void atualizar(
+            Long disponibilidadeId,
+            DisponibilidadeRequestDTO dto
+    ) {
+
+        Disponibilidade disponibilidadeExistente =
+                disponibilidadeRepository.findById(disponibilidadeId)
+                        .orElseThrow(() ->
+                                new RegraDeNegocioException(
+                                        "Disponibilidade não encontrada"
+                                ));
+
+        Usuario professor = usuarioRepository
+                .findById(dto.getProfessorId())
+                .orElseThrow(() ->
+                        new UsuarioNaoEncontradoException(
+                                "Professor não encontrado"
+                        ));
+
+        if (dto.getHoraInicio().isAfter(dto.getHoraFim())
+                || dto.getHoraInicio().equals(dto.getHoraFim())) {
+
+            throw new RegraDeNegocioException(
+                    "Hora início deve ser menor que hora fim"
+            );
+        }
+
+        if (dto.getDuracaoAula() <= 0) {
+            throw new RegraDeNegocioException(
+                    "Duração da aula inválida"
+            );
+        }
+
+        Disponibilidade disponibilidadeAtualizada =
+                Disponibilidade.builder()
+                        .id(disponibilidadeExistente.getId())
+                        .professor(professor)
+                        .diaSemana(dto.getDiaSemana())
+                        .horaInicio(dto.getHoraInicio())
+                        .horaFim(dto.getHoraFim())
+                        .duracaoAula(dto.getDuracaoAula())
+                        .build();
+
+        disponibilidadeRepository.save(
+                disponibilidadeAtualizada
+        );
     }
 }
+
