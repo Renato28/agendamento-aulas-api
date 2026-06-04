d7a8c790455dba86cccb4fd0d0f334b2dac0a04c
Renato Nobrega <renatonobrega87@gmail.com>
2026-06-04 15:30:56 -0300

    implementando endpoint para atualizar uma avaliacao

--- src/main/java/br/com/github/renato28/agendamentoaulaapi/controller/AvaliacaoController.java
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/controller/AvaliacaoController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/avaliacoes")
@@ -27,5 +24,13 @@ public class AvaliacaoController {
 
         return ResponseEntity
                 .status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable Long id, @RequestBody @Valid AvaliacaoRequestDTO dto) {
+        avaliacaoService.atualizar(id, dto);
+
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT).build();
     }
 }
