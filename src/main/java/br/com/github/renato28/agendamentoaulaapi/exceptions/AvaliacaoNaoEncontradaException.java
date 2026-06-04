e2a52581fcbc8d3d83f2796240f5b822728985da
Renato Nobrega <renatonobrega87@gmail.com>
2026-06-04 15:29:44 -0300

    implementacao classe de exception de avaliacao

---
+++ src/main/java/br/com/github/renato28/agendamentoaulaapi/exceptions/AvaliacaoNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package br.com.github.renato28.agendamentoaulaapi.exceptions;
+
+public class AvaliacaoNaoEncontradaException extends RuntimeException {
+    public AvaliacaoNaoEncontradaException(String message) {
+        super(message);
+    }
+}
